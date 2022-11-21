b3294703b8305c2d7f82a4979c8743b92a472d98
fatlillah <dwifatlillah13@gmail.com>
2022-11-21 09:29:58 +0700

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.layout_auth')
+@section('title', "Login")
+@section("content")
+<div class="container">
+<div class="row" style="padding-top: 50px">
+<div class="col-md-4"></div>
+<div class="col-md-4">
+<div class="form-login" style="text-align: center;">
+<h4>Login</h4>
+<!-- @if(session()->has('success'))
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+  {{ session('success') }}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif -->
+</div>
+@if (session("success"))
+<div class="alert alert-success">{{ session('success') }}</div>
+@endif
+
+@if (session("loginError"))
+<div class="alert alert-danger">{{ session('loginError') }}
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif
+
+<form method="post" action="{{ route('login') }}">
+  @csrf
+<div class="form-group">
+<label>Email address</label>
+<input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="Name email" value="{{ old('email')}}" autofocus required>
+@error('email')
+<div class="invalid-feedback">
+      {{$message}}
+    </div>
+    @enderror
+</div>
+<div class="form-group">
+<label>Password</label>
+<input type="password" name="password" class="form-control" id="password" placeholder="Password" autofocus required>
+</div>
+<button type="submit" class="btn btn-primary">Login</button>
+</form>
+<br>
+belum punya akun ? <a href="{{ route('register') }}">Register</a>
+</div>
+</div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatakuliahController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// for mahasiswa
+Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa');
+
+Route::get('/insertMahasiswa', [MahasiswaController::class, 'insertMahasiswa'])->name('insert data');
+Route::post('/addMahasiswa', [MahasiswaController::class, 'addMahasiswa'])->name('add');
+
+Route::get('/tampilDataMahasiswa/{id}', [MahasiswaController::class, 'tampilDataMahasiswa'])->name('tampildata');
+Route::post('/updateDataMahasiswa/{id}', [MahasiswaController::class, 'updateDataMahasiswa'])->name('updatedata');
+
+Route::get('/deleteMahasiswa/{id}', [MahasiswaController::class, 'daleteMahasiswa'])->name('dalete');
+
+
+
+
+// for matakuliah
+Route::get('/matakuliah', [MatakuliahController::class, 'index'])->name('matakuliah');
+Route::get('/insertMatakuliah', [MatakuliahController::class, 'insertMatakuliah'])->name('insertMatakuliah');
+Route::post('/addMatakuliah', [MatakuliahController::class, 'addMatakuliah'])->name('addMatakuliah');
+
+Route::get('/tampilDataMatakuliah/{id}', [MatakuliahController::class, 'tampilDataMatakuliah'])->name('tampildata');
+Route::post('updateMatkul/{id}', [MatakuliahController::class, 'updateMatkul'])->name('updateMatkul');
+
+Route::get('/deleteMatakuliah/{id}', [MatakuliahController::class, 'delete'])->name('delete');
+
+
+//Login&Register
+Route::redirect('/', '/auth/login');
+Route::prefix('/auth')->group(function(){
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::get('logout', [ 'as' => 'logout', 'uses' => 'LoginController@logout']);
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'processRegister']);
+});
