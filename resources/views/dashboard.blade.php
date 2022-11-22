b0e8581782942a01b83ed6155e06c87c49583f52
bagusuntoro <untorobagus65@gmail.com>
2022-11-22 15:39:21 +0700

    add chart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -285,7 +285,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
       </div><!-- /.container-fluid -->
     </div>
     <div class="content">
-        <h1>Kelompok 1</h1>
+      <div>
+        <canvas id="myChart"></canvas>
+      </div>
+      
     </div>
 
     <!-- /.content -->
@@ -324,5 +327,41 @@ scratch. This page gets rid of all links and provides the needed markup only.
 let address = document.getElementById('tes')
 
 </script>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
+      
+      
+      
+      
+      
+      
+      
+      
+
 </body>
 </html>
