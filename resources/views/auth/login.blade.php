f19891d06e4babeb6283d15bdab11bd436750f91
fatlillah <dwifatlillah13@gmail.com>
2022-11-22 21:55:50 +0700

    Alert_fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,20 +7,20 @@
 <div class="col-md-4">
 <div class="form-login" style="text-align: center;">
 <h4>Login</h4>
-<!-- @if(session()->has('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-  {{ session('success') }}
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-@endif -->
 </div>
 @if (session("success"))
-<div class="alert alert-success">{{ session('success') }}</div>
+<div class="alert alert-success alert-dismissible fade show" role="alert">{{ session('success') }}
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
 @endif
 
 @if (session("loginError"))
-<div class="alert alert-danger">{{ session('loginError') }}
-<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">{{ session('loginError') }}
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
 </div>
 @endif
 
